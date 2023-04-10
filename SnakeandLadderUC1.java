a7e0f5b5e425f0432dea94c19d184fa9b1f59111
ks0510 <kshaikh051999@gmail.com>
2023-04-10 11:22:16 +0530

    Added program for start the game

--- SnakeandLadderUC1.java
+++ SnakeandLadderUC1.java
@@ -16,21 +16,10 @@ public class SnakeandLadderUC1 {
 	public static void main(String[] args) {
 
 		Player p1 = new Player();
-		int position = 0, count = 0;
+		int position = 0;
 
 		System.out.println("Start the game");
-
-		while (position != 100) {
-			count++;
-			int dicevalue = (int) Math.floor(Math.random() * 6 + 1);
-			position = position + dicevalue;
-			if ((position <= 95 && dicevalue == 5) || (position <= 96 && dicevalue == 4)
-					|| (position <= 97 && dicevalue == 3) || (position <= 98 && dicevalue == 2)
-					|| (position <= 99 && dicevalue == 1)) {
-				position += dicevalue;
-			}
-		}
-
-		System.out.println("Player p1 win the game after " + count + " dice rolls");
-	}
+                
+                System.out.println("Position of player "+p1+" is"+position);
+        }
 }
